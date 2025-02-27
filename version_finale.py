48d2ef306c0e2aa5d4c6accb9443481bb0e5588b
Enzo Avagliano <eavagliano6@gmail.com>
2025-02-27 12:43:08 +0100

    Added a lot of comments for teachers

--- version_finale.py
+++ version_finale.py
@@ -7,22 +7,21 @@ import requests
 
 print("Bienvenue dans votre calculateur de position de planètes !")
 # Demander à l'utilisateur de choisir une date pour laquelle calculer les positions des planètes
-date = input("Entrez une date (format: YYYY-MM-DD): ")
-# date = "2025-02-27"
+date = input("Entrez une date au format année-mois-jour (Exemple: 2025-02-27): ") or "now" # Date choisir par l'utilisateur (maintenant par défaut si laissé vide)
 
-# Appeler le site internet IMCCE pour obtenir les données de toutes les planètes de la liste à la date choisie
-liste_planetes = ["Mercury", "Venus", "Mars", "Jupiter", "Saturn"] # Nom des planètes en anglais (OBLIGATOIRE)
-lien = "https://ssp.imcce.fr/webservices/miriade/api/ephemph.php"
+liste_planetes = ["Mercury", "Venus", "Mars", "Jupiter", "Saturn"] # Nom des planètes (en anglais OBLIGATOIRE pour le site)
+lien = "https://ssp.imcce.fr/webservices/miriade/api/ephemph.php" # Lien du site de calcul des positions de l'IMCCE
 
-print(f"\nVoici les résultats pour la date du {date}:\n")
-for planete in liste_planetes:
+print(f"\nVoici les résultats pour la date du {date}:")
+for planete in liste_planetes: # le mot-clé 'for' indique une boucle, les instructions à l'intérieur vont se répéter pour chaque nom dans la liste de planètes
     parametres = {
         "-name": f"p:{planete}", # La planete
         "-ep": date, # La date que nous avons choisie
         "-mime": "json", # Le format de la réponse (OBLIGATOIRE COMME CA)
-        "-output": "--coord(ec)", # Le format des coordonées (OBLIGATOIRE COMME CA) : ec = écliptique
-        "-observer": "@0"  # BARYCENTRE DU SYSTEME SOLAIRE
+        "-output": "--coord(ec)", # Le format des coordonées (OBLIGATOIRE) : ec = écliptique; eq2000 = équatorial
     }
+    # Appeler le site internet IMCCE pour obtenir les données de toutes les planètes de la liste à la date choisie
+    # On donne à la fonction requests la méthode GET (obligatoire), le lien du site et les paramètres (nom de la planète, date, format de coordonées)
     reponse = requests.request("GET", lien, params=parametres)
     data = reponse.json()
     longitude = data.get("data", [{}])[0].get("Longitude", "Erreur")
@@ -32,4 +31,4 @@ for planete in liste_planetes:
     else:
         print(f"- {planete} : {longitude[2:4]}°")
 
-print("\n\nMerci d'avoir utilisé notre calculateur de position de planètes !")
+print("\nMerci d'avoir utilisé notre calculateur de position de planètes !")
